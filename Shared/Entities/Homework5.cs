4ae6ab5b39d7d3c9180a7e2abbe3f4698c7345f9
AdiScheinoowitz <108361743+AdiScheinoowitz@users.noreply.github.com>
2022-11-26 21:21:08 +0200

    Homework5 complete
    Homework5 complete

---
+++ Shared/Entities/Homework5.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Homework2.Shared.Rectagle
+{
+    public class MyRectagle
+    {
+        public string Color { get; set; }
+        public double Length { get; set; }
+        public double Width { get; set; }
+        public double Surface()
+        {
+            return Length * Width;
+        }
+        public double Scope()
+        {
+            return 2 * (Width + Length);
+        }
+    }
+}
